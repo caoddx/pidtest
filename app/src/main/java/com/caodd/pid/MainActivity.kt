e602fb714c3e66410698491eaedce85865d022cd
caodd <caodd1993@qq.com>
2018-07-07 00:43:40 +0800

    增加操作便捷性

--- app/src/main/java/com/caodd/pid/MainActivity.kt
+++ app/src/main/java/com/caodd/pid/MainActivity.kt
@@ -13,10 +13,15 @@ import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var lastSet: LineDataSet? = null
+    private val colors = mapOf(
+            "histories" to Color.RED,
+            "valves" to Color.BLUE,
+            "outs" to Color.GREEN,
+            "sums" to Color.BLACK
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,46 +30,83 @@ class MainActivity : AppCompatActivity() {
 
         lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
 
-        val colors = mapOf(
-                "histories" to Color.RED,
-                "valves" to Color.BLUE,
-                "outs" to Color.GREEN,
-                "sums" to Color.BLACK
-        )
+        var plus = editTextPlus.text.toString().toFloat()
+
+        buttonMinus.setOnClickListener {
+            plus -= 0.1f
+            if (plus < 0f) plus = 0f
+            editTextPlus.setText(String.format("%.2f", plus))
+        }
+        buttonPlus.setOnClickListener {
+            plus += 0.1f
+            editTextPlus.setText(String.format("%.2f", plus))
+        }
+        buttonKpPlus.setOnClickListener {
+            val f = editKp.text.toString().toFloat()
+            editKp.setText((f + plus).toString())
+        }
+        buttonKpMinus.setOnClickListener {
+            val f = editKp.text.toString().toFloat()
+            editKp.setText((f - plus).toString())
+        }
+        buttonKiPlus.setOnClickListener {
+            val f = editKi.text.toString().toFloat()
+            editKi.setText((f + plus).toString())
+        }
+        buttonKiMinus.setOnClickListener {
+            val f = editKi.text.toString().toFloat()
+            editKi.setText((f - plus).toString())
+        }
+        buttonKdPlus.setOnClickListener {
+            val f = editKd.text.toString().toFloat()
+            editKd.setText((f + plus).toString())
+        }
+        buttonKdMinus.setOnClickListener {
+            val f = editKd.text.toString().toFloat()
+            editKd.setText((f - plus).toString())
+        }
+
+        var lastSet: LineDataSet? = null
 
         button.setOnClickListener {
             val kp = editKp.text.toString().toFloat()
-            val ti = editTi.text.toString().toFloat()
-            val td = editTd.text.toString().toFloat()
+            val ti = editKi.text.toString().toFloat()
+            val td = editKd.text.toString().toFloat()
 
             // 10 8 1
-            val pid = SimplifyPositionPid(kp, ti, td)
+            val pid: Pid = SimplifyPositionPid(kp, ti, td)
 
-            val map = update(pid, 1f, 0f, 0.5f) { current, valve ->
-                current + valve * 0.1f
-            }
-
-            val setMap = map.mapValues { (name, list) ->
-                makeSet(name, list, colors[name]!!)
-            }.filter { it.key != "sums" }
-
-            /*val lineData = if (lastSet != null) {
-                lastSet?.color = Color.GRAY
-                lastSet?.label = "last histories"
-                LineData(setMap.values + lastSet)
-            } else {
-                LineData(setMap.values.toList())
-            }*/
-
+            val setMap = updateMap(pid)
+            val set = setMap["histories"]!!
+            set.label = pid.toString().filter { it.isLetter().not() && it != '=' }
             val lineData = LineData(setMap.values.toList())
 
-            //lastSet = setMap["histories"]
+            lastSet?.let {
+                it.color = 0xD0888888.toInt()
+                lineData.addDataSet(it)
+            }
+            lastSet = set
 
             lineChart.data = lineData
             lineChart.animateX(1000)
-
-
         }
+    }
+
+    private fun updateMap(pid: Pid): Map<String, LineDataSet> {
+        val vs = LinkedList<Float>()
+
+        for (i in 0..19) {
+            vs.offer(0.5f)
+        }
+
+        val map = update(pid, 1f, 1f) { current, valve ->
+            vs.offer(valve)
+            current + (vs.poll() - 0.5f) * 0.1f
+        }
+
+        return map.mapValues { (name, list) ->
+            makeSet(name, list, colors[name]!!)
+        }.filter { it.key != "sums" }
     }
 
     private fun update(pid: Pid, vararg targets: Float, applyValve: (current: Float, valve: Float) -> Float): Map<String, List<Float>> {
@@ -88,34 +130,28 @@ class MainActivity : AppCompatActivity() {
         for (i in 0..19) {
             pid.updateError(0f)
             out = pid.getOut()
-            valve += out
+            valve = (out + 1) / 2
+            current = applyValve(current, valve)
             upList()
         }
 
         targets.forEach {
             val target = it
 
-            for (i in 0..499) {
-
+            for (i in 0..299) {
                 pid.updateError(target - current)
                 out = pid.getOut()
-
-                valve = out
-
-                if (valve > 1.0f) valve = 1.0f
-                if (valve < -1.0f) valve = -1.0f
-
+                valve = (out + 1) / 2
                 current = applyValve(current, valve)
-
                 upList()
             }
         }
 
         return mapOf(
-                "histories" to histories.toList(),
+                "sums" to sums.toList(),
                 "valves" to valves.toList(),
                 "outs" to outs.toList(),
-                "sums" to sums.toList()
+                "histories" to histories.toList()
         )
     }
 
